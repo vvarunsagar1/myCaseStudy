83a08e713ab88c018303cae2b05b346612965d5e
Developer 365Innovative <dev.365innovative@gmail.com>
2017-06-10 23:10:28 +0530

    With Functionality

--- admin/js/app.js
+++ admin/js/app.js
@@ -3,65 +3,91 @@ var app = angular.module('myCaseStudyAdmin',[]);
 function signOut() {
    var auth2 = gapi.auth2.getAuthInstance();
    auth2.signOut().then(function () {
-     console.log('User signed out.');
+     //console.log('User signed out.');
    });
    window.localStorage['profile'] = '';
  }
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  // console.log('ID: ' + profile.getId());
-  // console.log('Name: ' + profile.getName());
-  // console.log('Given Name: ' + profile.getGivenName());
-  // console.log('Family Name: ' + profile.getFamilyName());
-  // console.log('Image URL: ' + profile.getImageUrl());
-  // console.log('Email: ' + profile.getEmail());
+  // //console.log('ID: ' + profile.getId());
+  // //console.log('Name: ' + profile.getName());
+  // //console.log('Given Name: ' + profile.getGivenName());
+  // //console.log('Family Name: ' + profile.getFamilyName());
+  // //console.log('Image URL: ' + profile.getImageUrl());
+  // //console.log('Email: ' + profile.getEmail());
   var profileUser = { 'id': profile.getId(), 'name': profile.getName(), 'imageurl': profile.getImageUrl(), 'email': profile.getEmail() };
-  // console.log('profileUser', profileUser);
+  // //console.log('profileUser', profileUser);
   window.localStorage['profile'] = JSON.stringify(profileUser);
-  // console.log(window.localStorage['profileUser'],'profile');
-  console.log('Extracted Profile', JSON.parse(window.localStorage['profile']));
+  // //console.log(window.localStorage['profileUser'],'profile');
+  //console.log('Extracted Profile', JSON.parse(window.localStorage['profile']));
   window.location.href = 'index.html';
 }
 
 app.run(function($rootScope, $http) {
-  console.log('Admin App');
+  //console.log('Admin App');
   $rootScope.server="http://localhost/myCaseStudy/admin/api/";
   $rootScope.getIpURL = $rootScope.server+'get_ip.php';
+
   $http.get($rootScope.getIpURL).then(function(res) {
     $rootScope.ip = res.data.ip;
-    console.log('ip',$rootScope.ip);
+    //console.log('ip',$rootScope.ip);
     $rootScope.monitorVisitorsURL = $rootScope.server + 'monitor_visitors.php?ip=' + $rootScope.ip;
     $http.get($rootScope.monitorVisitorsURL).then(function(res){
       $rootScope.monitorStats = res;
-      console.log('monitorStats', $rootScope.monitorStats);
+      //console.log('monitorStats', $rootScope.monitorStats);
     })
   })
+
+  $rootScope.logout = function () {
+    window.localStorage['profile'] = '';
+    window.location.href = 'index.html';
+  }
+
 });
 
 app.controller('loginController', function ($rootScope, $scope, $http) {
-  console.log('loginController');
+  //console.log('loginController');
+  $scope.login = function (loginProfile) {
+    $scope.loginProfile = loginProfile;
+    $http.get($rootScope.getIpURL).then(function(res) {
+      $rootScope.ip = res.data.ip;
+      //console.log('ip',$rootScope.ip);
+      $rootScope.loginURL = $rootScope.server + 'login.php?ip=' + $rootScope.ip + '&email=' + $scope.loginProfile.email_id + '&password=' + $scope.loginProfile.password ;
+      //console.log($rootScope.loginURL);
+      $http.get($rootScope.loginURL).then(function(res){
+        $rootScope.profile = res.data;
+        if ($rootScope.profile.login_id != null || $rootScope.profile.login_id != undefined || $rootScope.profile.login_id != '' ){
+          //console.log('profile', $rootScope.profile);
+          window.localStorage['profile'] = JSON.stringify($rootScope.profile);
+          window.location.href = 'index.html';
+        } else if ($rootScope.profile.page == 'login.php'){
+          //console.log('message', $rootScope.profile.message);
+        }
+      })
+    })
+  }
 });
 
 app.controller('AdminController', function ($rootScope, $scope, $http) {
-  console.log('AdminController');
-  console.log(window.localStorage['profile']);
+  //console.log('AdminController');
+  //console.log(window.localStorage['profile']);
   if (window.localStorage['profile'] == '' || window.localStorage['profile'] == null || window.localStorage['profile'] == undefined ) {
     window.location.href = 'login.html';
-    console.log('redirection');
+    //console.log('redirection');
   }
   $rootScope.profile = JSON.parse(window.localStorage['profile']);
-  console.log('profile', $rootScope.profile);
+  //console.log('profile', $rootScope.profile);
 
   $scope.fetchVisitors = function () {
     $http.get($rootScope.getIpURL).then(function(res) {
       $rootScope.ip = res.data.ip;
-      console.log('ip',$rootScope.ip);
+      //console.log('ip',$rootScope.ip);
       $scope.fetchVisitorsURL = $rootScope.server + "fetch_visitors.php?ip="+ $rootScope.ip;
-      console.log($scope.fetchVisitorsURL);
+      //console.log($scope.fetchVisitorsURL);
       $http.get($scope.fetchVisitorsURL).then(function(response) {
         $scope.visitors = response.data;
-        console.log('visitorsResponse', $scope.visitors);
+        //console.log('visitorsResponse', $scope.visitors);
       });
     })
   }
@@ -70,12 +96,12 @@ app.controller('AdminController', function ($rootScope, $scope, $http) {
   $scope.fetchEnquiries = function () {
     $http.get($rootScope.getIpURL).then(function(res) {
       $rootScope.ip = res.data.ip;
-      console.log('ip',$rootScope.ip);
+      //console.log('ip',$rootScope.ip);
       $scope.fetchEnquiryURL = $rootScope.server + "fetch_enquiries.php?ip="+ $rootScope.ip;
-      console.log($scope.fetchEnquiryURL);
+      //console.log($scope.fetchEnquiryURL);
       $http.get($scope.fetchEnquiryURL).then(function(response) {
         $scope.enquiries = response.data;
-        console.log('enquiriesResponse', $scope.enquiries);
+        //console.log('enquiriesResponse', $scope.enquiries);
       });
     })
   }
--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 var app = angular.module('myCaseStudy',[]);
 
 app.run(function ($rootScope,$http) {
-  console.log('App Starts');
+  //console.log('App Starts');
   $rootScope.server="http://localhost/myCaseStudy/admin/api/";
   $rootScope.getIpURL = $rootScope.server+'get_ip.php';
   $rootScope.mobileMenu = 'hide';
   $http.get($rootScope.getIpURL).then(function(res) {
     $rootScope.ip = res.data.ip;
-    console.log('ip',$rootScope.ip);
+    //console.log('ip',$rootScope.ip);
     $rootScope.monitorVisitorsURL = $rootScope.server + 'monitor_visitors.php?ip=' + $rootScope.ip;
     $http.get($rootScope.monitorVisitorsURL).then(function(res){
       $rootScope.monitorStats = res;
-      console.log('monitorStats', $rootScope.monitorStats);
+      //console.log('monitorStats', $rootScope.monitorStats);
     })
   })
 
@@ -19,7 +19,7 @@ app.run(function ($rootScope,$http) {
     $rootScope.serviceItemView = view;
   }
   $rootScope.toggleMobileMenu = function () {
-    console.log('toggle');
+    //console.log('toggle');
     if ($rootScope.mobileMenu == 'hide') {
       $rootScope.mobileMenu = 'active';
     } else if ($rootScope.mobileMenu == 'active') {
@@ -29,17 +29,17 @@ app.run(function ($rootScope,$http) {
 });
 
 app.controller('HomeController', function($scope,$rootScope,$http) {
-  console.log('HomeController');
+  //console.log('HomeController');
 
   $scope.addNewEnquiry = function (formData) {
     $http.get($rootScope.getIpURL).then(function(res) {
       $rootScope.ip = res.data.ip;
-      console.log('ip',$rootScope.ip);
+      //console.log('ip',$rootScope.ip);
       $scope.addNewEnquiryURL = $rootScope.server + 'insert_enquiry.php?ip=' + $rootScope.ip +'&name=' + formData.name +'&email=' + formData.email +'&message=' + formData.message ;
-      console.log($scope.addNewEnquiryURL);
+      //console.log($scope.addNewEnquiryURL);
       $http.get($scope.addNewEnquiryURL).then(function(res){
         $scope.enquiryResponse = res;
-        console.log('enquiryResponse', $scope.enquiryResponse);
+        //console.log('enquiryResponse', $scope.enquiryResponse);
       })
     })
 
@@ -48,13 +48,13 @@ app.controller('HomeController', function($scope,$rootScope,$http) {
   $scope.fetchVisitors = function () {
     $http.get($rootScope.getIpURL).then(function(res) {
       $rootScope.ip = res.data.ip;
-      console.log('ip',$rootScope.ip);
+      //console.log('ip',$rootScope.ip);
       $scope.fetchVisitorsURL = $rootScope.server + "fetch_visitors.php?ip="+ $rootScope.ip;
-      console.log($scope.fetchVisitorsURL);
+      //console.log($scope.fetchVisitorsURL);
       $http.get($scope.fetchVisitorsURL).then(function(response) {
         $scope.visitors = response.data;
-        console.log('visitorsResponse', $scope.visitors);
-        console.log('length',$scope.visitors.length);
+        //console.log('visitorsResponse', $scope.visitors);
+        //console.log('length',$scope.visitors.length);
       });
     })
   }
@@ -63,12 +63,12 @@ app.controller('HomeController', function($scope,$rootScope,$http) {
   $scope.fetchEnquiries = function () {
     $http.get($rootScope.getIpURL).then(function(res) {
       $rootScope.ip = res.data.ip;
-      console.log('ip',$rootScope.ip);
+      //console.log('ip',$rootScope.ip);
       $scope.fetchEnquiryURL = $rootScope.server + "fetch_enquiries.php?ip="+ $rootScope.ip;
-      console.log($scope.fetchEnquiryURL);
+      //console.log($scope.fetchEnquiryURL);
       $http.get($scope.fetchEnquiryURL).then(function(response) {
         $scope.enquiries = response.data;
-        console.log('enquiriesResponse', $scope.enquiries);
+        //console.log('enquiriesResponse', $scope.enquiries);
       });
     })
   }
@@ -77,12 +77,12 @@ app.controller('HomeController', function($scope,$rootScope,$http) {
   $scope.fetchViews = function () {
     $http.get($rootScope.getIpURL).then(function(res) {
       $rootScope.ip = res.data.ip;
-      console.log('ip',$rootScope.ip);
+      //console.log('ip',$rootScope.ip);
       $scope.fetchViewsURL = $rootScope.server + "fetch_views.php?ip="+ $rootScope.ip;
-      console.log($scope.fetchViewsURL);
+      //console.log($scope.fetchViewsURL);
       $http.get($scope.fetchViewsURL).then(function(response) {
         $scope.views = response.data[0].views;
-        console.log('viewsResponse', $scope.views);
+        //console.log('viewsResponse', $scope.views);
       });
     })
   }
